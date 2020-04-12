d74123f1482df69b1af53cdc96c52739b952cc83
Steven Nooijen <stevennooijen@godatadriven.com>
2020-04-12 09:11:45 +0200

    Add seed, offset and n_results to Explore api

--- api/resources/explore.py
+++ api/resources/explore.py
@@ -3,11 +3,13 @@ import pandas as pd
 from flask_restful import Resource, reqparse
 
 parser = reqparse.RequestParser()
+parser.add_argument('seed', type=int, default=1234)  # should be required? Otherwise always same results.
+parser.add_argument('offset', type=int, default=0)
+parser.add_argument('n_results', type=int, default=10)
 parser.add_argument('ne_lat', type=float)
 parser.add_argument('ne_lng', type=float)
 parser.add_argument('sw_lat', type=float)
 parser.add_argument('sw_lng', type=float)
-
 
 def filter_on_geolocation(df_in, ne_lat, ne_lng, sw_lat, sw_lng):
     return (
@@ -32,8 +34,8 @@ class Explore(Resource):
                 subset = (
                     self.df
                     .pipe(filter_on_geolocation, args['ne_lat'], args['ne_lng'], args['sw_lat'], args['sw_lng'])
-                    .sample(frac=1)
-                    .head(10)
+                    .sample(frac=1, random_state=args['seed'])
+                    .iloc[args['offset'] : args['offset'] + args['n_results']]
                 ).to_dict(orient='records')
             except ValueError:
                 subset = []
@@ -43,8 +45,9 @@ class Explore(Resource):
             # if no dest_id in url, then set random
             subset = (
                 self.df
-                .sample(frac=1)
-                .head(10)
+                .sample(frac=1, random_state=args['seed'])
+                # .head(N_RESULTS)
+                .iloc[args['offset']: args['offset'] + args['n_results']]
             ).to_dict(orient='records')
 
         return {"Destinations": subset}
