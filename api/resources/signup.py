7587285ff8f5af7f8ca31be46f469a8dd2d91211
Steven Nooijen <stevennooijen@godatadriven.com>
2020-06-06 12:05:30 +0200

    Split likes html into multiple merge fields + black formatting

--- api/resources/signup.py
+++ api/resources/signup.py
@@ -2,66 +2,81 @@ import pandas as pd
 from json import load
 from requests import post, get, patch
 from flask_restful import Resource, reqparse
-from resources.utils.utils import create_list_of_likes
+from resources.utils.utils import (
+    create_list_of_likes,
+    split_text_in_chuncks,
+    pad_or_truncate_list,
+)
 from resources.utils.interests import create_interests_query
 
 # TODO split into 2 separate parsers, one for query and one for payload/body
 # Reason is that you cant set location argument required to True for GET requests
 parser = reqparse.RequestParser()
-parser.add_argument('email', type=str, required=True)
-parser.add_argument('location', type=str, required=False)
-parser.add_argument('status', type=str, required=False, default='subscribed')
-parser.add_argument('marketing', type=bool, required=False)
-parser.add_argument('likes', type=int, required=False, action='append')
-parser.add_argument('flights', type=bool, required=False, default=False)
-parser.add_argument('local_transport', type=bool, required=False, default=False)
-parser.add_argument('activities', type=bool, required=False, default=False)
-parser.add_argument('accommodation', type=bool, required=False, default=False)
-parser.add_argument('none', type=bool, required=False, default=False)
+parser.add_argument("email", type=str, required=True)
+parser.add_argument("location", type=str, required=False)
+parser.add_argument("status", type=str, required=False, default="subscribed")
+parser.add_argument("marketing", type=bool, required=False)
+parser.add_argument("likes", type=int, required=False, action="append")
+parser.add_argument("flights", type=bool, required=False, default=False)
+parser.add_argument("local_transport", type=bool, required=False, default=False)
+parser.add_argument("activities", type=bool, required=False, default=False)
+parser.add_argument("accommodation", type=bool, required=False, default=False)
+parser.add_argument("none", type=bool, required=False, default=False)
 
 
 class Signup(Resource):
     def __init__(self, **kwargs):
-        with open(kwargs['mailchimp-key'], 'r') as f:
+        with open(kwargs["mailchimp-key"], "r") as f:
             self.key = load(f)
-        self.df = (
-            pd.read_csv('./data/wikivoyage_destinations.csv')
-        )
+        self.df = pd.read_csv("./data/wikivoyage_destinations.csv")
 
     def post(self):
         args = parser.parse_args()
 
-        payload = {'email_address': args['email'],
-                   'status': args['status'],
-                   'merge_fields': {
-                       "SIGNUP": args['location']
-                   }}
+        payload = {
+            "email_address": args["email"],
+            "status": args["status"],
+            "merge_fields": {"SIGNUP": args["location"]},
+        }
 
         # if opt-in for marketing provided, update that merge field
-        if args['marketing']:
-            payload['merge_fields']['MARKETING'] = 'Yes, sign me up!'
+        if args["marketing"]:
+            payload["merge_fields"]["MARKETING"] = "Yes, sign me up!"
 
         # if likes are provided, the booking arguments should also be there
-        if args['likes']:
-            payload['merge_fields']['UTIL_HTML'] = create_list_of_likes(self.df, args['likes'])
+        if args["likes"]:
+            util_html = create_list_of_likes(self.df, args["likes"], add_link=True)
+            for i, html_part in enumerate(
+                pad_or_truncate_list(list(split_text_in_chuncks(util_html)))
+            ):
+                # for this to work, 'merge_field' should already exist in the payload dict
+                payload["merge_fields"][f"UTIL{i}"] = html_part
             user_interests = {
-                'flights': args['flights'],
-                'local_transport': args['local_transport'],
-                'activities': args['activities'],
-                'accommodation': args['accommodation'],
-                'none': args['none'],
+                "flights": args["flights"],
+                "local_transport": args["local_transport"],
+                "activities": args["activities"],
+                "accommodation": args["accommodation"],
+                "none": args["none"],
             }
-            payload['merge_fields']['BOOKING'] = 'Yes' if args['none'] == False else 'No'
-            payload['interests'] = create_interests_query(user_interests)
+            payload["merge_fields"]["BOOKING"] = (
+                "Yes" if args["none"] == False else "No"
+            )
+            payload["interests"] = create_interests_query(user_interests)
 
-        headers = {'content-type': "application/json"}
+        headers = {"content-type": "application/json"}
 
         # Use auth for Basic authentication
-        r = post('https://us3.api.mailchimp.com/3.0/lists/{}/members/'.format(self.key['audience_id']),
-                 json=payload, headers=headers, auth=('randomstring', self.key['api_key']))
+        r = post(
+            "https://us3.api.mailchimp.com/3.0/lists/{}/members/".format(
+                self.key["audience_id"]
+            ),
+            json=payload,
+            headers=headers,
+            auth=("randomstring", self.key["api_key"]),
+        )
 
         try:
-            member_id = r.json()['id']
+            member_id = r.json()["id"]
         except KeyError:
             member_id = None
 
@@ -70,11 +85,15 @@ class Signup(Resource):
     def get(self):
         args = parser.parse_args()
 
-        r = get('https://us3.api.mailchimp.com/3.0/lists/{}/members/{}'.format(self.key['audience_id'], args['email']),
-                auth=('randomstring', self.key['api_key']))
+        r = get(
+            "https://us3.api.mailchimp.com/3.0/lists/{}/members/{}".format(
+                self.key["audience_id"], args["email"]
+            ),
+            auth=("randomstring", self.key["api_key"]),
+        )
 
         try:
-            member_id = r.json()['id']
+            member_id = r.json()["id"]
         except KeyError:
             member_id = None
 
@@ -84,28 +103,40 @@ class Signup(Resource):
         args = parser.parse_args()
 
         # default status should be 'subscribed'. Otherwise no emails can be sent.
-        payload = {"status": args['status']}
+        payload = {"status": args["status"]}
 
-        if args['marketing']:
-            payload = {"merge_fields": {'MARKETING': 'Yes, sign me up!'}}
+        if args["marketing"]:
+            payload = {"merge_fields": {"MARKETING": "Yes, sign me up!"}}
         # if likes are provided, the booking arguments should also be there
-        elif args['likes']:
-            payload = {"merge_fields": {'UTIL_HTML': create_list_of_likes(self.df, args['likes'])}}
-            user_interests = {
-                'flights': args['flights'],
-                'local_transport': args['local_transport'],
-                'activities': args['activities'],
-                'accommodation': args['accommodation'],
-                'none': args['none'],
+        elif args["likes"]:
+            payload = {
+                "merge_fields": {"BOOKING": "Yes" if args["none"] == False else "No"}
             }
-            payload['merge_fields']['BOOKING'] = 'Yes' if args['none'] == False else 'No'
-            payload['interests'] = create_interests_query(user_interests)
+            user_interests = {
+                "flights": args["flights"],
+                "local_transport": args["local_transport"],
+                "activities": args["activities"],
+                "accommodation": args["accommodation"],
+                "none": args["none"],
+            }
+            payload["interests"] = create_interests_query(user_interests)
+            util_html = create_list_of_likes(self.df, args["likes"], add_link=True)
+            for i, html_part in enumerate(
+                pad_or_truncate_list(list(split_text_in_chuncks(util_html)))
+            ):
+                # for this to work, 'merge_field' should already exist in the payload dict
+                payload["merge_fields"][f"UTIL{i}"] = html_part
 
-        headers = {'content-type': "application/json"}
+        headers = {"content-type": "application/json"}
         # Use auth for Basic authentication
-        r = patch('https://us3.api.mailchimp.com/3.0/lists/{}/members/{}'.format(self.key['audience_id'],
-                                                                                 args['email']),
-                  json=payload, headers=headers, auth=('randomstring', self.key['api_key']))
+        r = patch(
+            "https://us3.api.mailchimp.com/3.0/lists/{}/members/{}".format(
+                self.key["audience_id"], args["email"]
+            ),
+            json=payload,
+            headers=headers,
+            auth=("randomstring", self.key["api_key"]),
+        )
 
         # always successful given valid status input, so return doesn't matter
         return r.status_code
--- api/resources/utils/utils.py
+++ api/resources/utils/utils.py
@@ -1,17 +1,122 @@
-def create_one_list_item(df_in, place_id, line_prefix='<li>', line_postfix='.'):
-    record = df_in.loc[lambda df: df['id'] == place_id]
-    line = line_prefix + record['name'].iloc[0] + ', ' + record['country'].iloc[0] + line_postfix
+def create_stairwaytotravel_url(place_id):
+    """Create a url to a place page given the place id."""
+    return "https://stairwaytotravel.com/explore/" + str(place_id)
+
+
+def create_html_link(url, text):
+    """Wrap a text into an html anchor element with a specified url."""
+    line = f"<a href='{url}'>{text}</a>"
     return line
 
-def create_list_of_likes(df_in, likes, list_prefix='<ul>', list_postfix='</ul>', **kwargs):
+
+def create_one_list_item(
+    df_in, place_id, line_prefix="<li>", line_postfix="", add_link=False
+):
+    """Create one line of text for a given place.
+
+    Parameters
+    ----------
+    df_in: DataFrame
+        DataFrame to look up the place_id in.
+    place_id: int
+        Unique id for the place to generate the line of text for.
+    line_prefix: str
+        Start each line with this string.
+    line_postfix: str
+        End each line with this string.
+    add_link: bool
+        Whether to wrap the text in an html anchor element.
+    Returns
+    -------
+    str
+        A line of text about the specified place.
+    """
+    record = df_in.loc[lambda df: df["id"] == place_id]
+    text = record["name"].iloc[0] + ", " + record["country"].iloc[0]
+    link = create_html_link(create_stairwaytotravel_url(place_id), text)
+    line = line_prefix + (link if add_link else text) + line_postfix
+    return line
+
+
+def create_list_of_likes(
+    df_in, likes, list_prefix="<ul>", list_postfix="</ul>", **kwargs
+):
+    """Create a string listing the specified liked places.
+
+    Parameters
+    ----------
+    df_in: DataFrame
+        DataFrame to look up the place_id in.
+    likes: list
+        List with unique ids for the places to create the text for.
+    list_prefix: str
+        Start the list with this string.
+    list_postfix: str
+        End the list with this string.
+    Returns
+    -------
+    str
+        A string listing the specified places.
+    """
     output = list_prefix
     for like in likes:
         output = output + create_one_list_item(df_in, like, **kwargs)
     output = output + list_postfix
     return output
 
+
+def split_text_in_chuncks(text, max_characters=255, split_character=">"):
+    """Generator that chops a text into parts that end with a certain character.
+
+    Parameters
+    ----------
+    text: str
+        Input string to be split into parts.
+    max_characters: int
+        Maximum length of one part.
+    split_character: str
+        Character that each part should end with.
+    Returns
+    -------
+    iterator
+        Generator that yields parts of the string.
+    """
+    remaining_text = text
+    while True:
+        next_part = remaining_text[0:max_characters]
+        index_in_chunck = next_part.rfind(split_character)
+        if index_in_chunck < 0:
+            break
+        yield remaining_text[0 : index_in_chunck + 1]
+        remaining_text = remaining_text[index_in_chunck + 1 :]
+
+
+def pad_or_truncate_list(some_list, target_len=21, pad_value=""):
+    """Pads (with a value) or truncates list to a target length.
+
+    Parameters
+    ----------
+    some_list: list
+        Input list to pad or truncate.
+    target_len: int
+        Length of the desired output list.
+    pad_value: anything
+        Value to pad with if the input length needs to be extended.
+    Returns
+    -------
+    list
+        Padded or truncated list of target length.
+    """
+    return some_list[:target_len] + [pad_value] * (target_len - len(some_list))
+
+
 def prettify_n_results(n):
     thresholds = [300, 1000, 5000, 10000, 20000]
     if n > 300:
-        n = '{:,}'.format((max([result for result in thresholds if n > result]))).replace(',', '.') + '+'
+        n = (
+            "{:,}".format(
+                (max([result for result in thresholds if n > result]))
+            ).replace(",", ".")
+            + "+"
+        )
     return n
