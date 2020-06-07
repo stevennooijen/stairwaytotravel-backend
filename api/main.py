7a78098be6b53e3e0b29216a0480cded701b5147
Steven Nooijen <stevennooijen@godatadriven.com>
2020-06-07 20:55:10 +0200

    Apply black formatting to main

--- api/main.py
+++ api/main.py
@@ -7,28 +7,50 @@ from resources.signup import Signup
 from resources.member import Member
 
 app = Flask(__name__)
-CORS(app, origins=["https://stairwaytotravel.com",
-                   "https://stairwaytotravel.web.app", "https://stairwaytotravel.firebaseapp.com",
-                   "https://stairwaytotravel-release.web.app", "https://stairwaytotravel-release.firebaseapp.com",
-                   "http://localhost:3000"])
+CORS(
+    app,
+    origins=[
+        "https://stairwaytotravel.com",
+        "https://stairwaytotravel.web.app",
+        "https://stairwaytotravel.firebaseapp.com",
+        "https://stairwaytotravel-release.web.app",
+        "https://stairwaytotravel-release.firebaseapp.com",
+        "http://localhost:3000",
+    ],
+)
 api = Api(app)
 
-FIRESTORE_KEY = 'credentials/stairway-firestore-key.json'
-MAILCHIMP_KEY = 'credentials/mailchimp-key.json'
-db_collection = 'destinations'
+FIRESTORE_KEY = "credentials/stairway-firestore-key.json"
+MAILCHIMP_KEY = "credentials/mailchimp-key.json"
+db_collection = "destinations"
 
 # Api for fetching destinations
-api.add_resource(Destination, '/api/',
-                 resource_class_kwargs={'firestore-key': FIRESTORE_KEY, 'db-collection': db_collection})
-api.add_resource(Destination, '/api/<dest_id>', endpoint='dest_ep',
-                 resource_class_kwargs={'firestore-key': FIRESTORE_KEY, 'db-collection': db_collection})
-api.add_resource(Explore, '/api/explore/')
+api.add_resource(
+    Destination,
+    "/api/",
+    resource_class_kwargs={
+        "firestore-key": FIRESTORE_KEY,
+        "db-collection": db_collection,
+    },
+)
+api.add_resource(
+    Destination,
+    "/api/<dest_id>",
+    endpoint="dest_ep",
+    resource_class_kwargs={
+        "firestore-key": FIRESTORE_KEY,
+        "db-collection": db_collection,
+    },
+)
+api.add_resource(Explore, "/api/explore/")
 
 # Api for email signup form
-api.add_resource(Signup, '/signup/',
-                 resource_class_kwargs={'mailchimp-key': MAILCHIMP_KEY})
-api.add_resource(Member, '/member/',
-                 resource_class_kwargs={'mailchimp-key': MAILCHIMP_KEY})
+api.add_resource(
+    Signup, "/signup/", resource_class_kwargs={"mailchimp-key": MAILCHIMP_KEY}
+)
+api.add_resource(
+    Member, "/member/", resource_class_kwargs={"mailchimp-key": MAILCHIMP_KEY}
+)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
