01981e69336f16275eb468ec0ff37d87eb14fe68
Steven Nooijen <stevennooijen@godatadriven.com>
2020-06-27 18:53:56 +0200

    Use profiles for sorting and selecting features

--- api/resources/explore.py
+++ api/resources/explore.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from flask_restful import Resource, reqparse
-from resources.utils.selection import filter_on_geolocation, select_top_features
+from resources.utils.selection import filter_on_geolocation
 from resources.utils.utils import prettify_n_results
+from resources.utils.features import (
+    select_features_with_profiles,
+    select_feature_columns_with_profiles,
+)
 
 parser = reqparse.RequestParser()
 parser.add_argument("seed", type=int, default=1234)
@@ -12,12 +16,16 @@ parser.add_argument("ne_lat", type=float)
 parser.add_argument("ne_lng", type=float)
 parser.add_argument("sw_lat", type=float)
 parser.add_argument("sw_lng", type=float)
+parser.add_argument("profiles", type=str, action="append", default=[])
 
 
 class Explore(Resource):
     def __init__(self):
-        self.df = pd.read_csv("./data/wikivoyage_destinations.csv")
+        self.df = pd.read_csv("./data/wikivoyage_destinations.csv").set_index(
+            "id", drop=False
+        )
         self.df_features = pd.read_csv("./data/wikivoyage_features.csv").set_index("id")
+        self.df_feature_types = pd.read_csv("./data/wikivoyage_features_types.csv")
 
     def get(self):
         args = parser.parse_args()
@@ -51,13 +59,30 @@ class Explore(Resource):
 
         # select records to output from subset
         try:
-            places = subset.sample(
-                frac=1, random_state=args["seed"], weights="weight"
-            ).iloc[args["offset"] : args["offset"] + args["n_results"]]
+            if args["profiles"]:
+                features_inscope = select_feature_columns_with_profiles(
+                    args["profiles"], self.df_feature_types
+                )
+                ordering = (
+                    self.df_features.loc[subset["id"].tolist()][features_inscope]
+                    .sum(axis=1)
+                    .sort_values(ascending=False)
+                    .index.values
+                )
+                places = subset.loc[ordering]
+            else:
+                places = subset.sample(
+                    frac=1, random_state=args["seed"], weights="weight"
+                )
+            places = places.iloc[args["offset"] : args["offset"] + args["n_results"]]
+
             # add top X features
             places["features"] = places["id"].apply(
-                lambda x: select_top_features(x, self.df_features)
+                lambda x: select_features_with_profiles(
+                    x, args["profiles"], self.df_features, self.df_feature_types
+                )
             )
+
             # return as dict
             places = places.to_dict(orient="records")
         # if subset is empty return empty list
---
+++ api/resources/utils/features.py
@@ -0,0 +1,90 @@
+def select_features(place_id, df_features, top_x=5, min_threshold=0.1):
+    """
+    Get top x most relevant features for a place.
+
+    Parameters
+    ----------
+    place_id: int
+        Place ID to return features for.
+    df_features: DataFrame
+        Data set with feature scores for all places.
+    top_x: int
+        Number of features to return in descending order of feature score.
+    min_threshold: float
+        Minimum score a feature needs to have.
+
+    Returns
+    -------
+    out: list
+        List with the top x feature names.
+    """
+    return (
+        df_features.loc[place_id]
+        .loc[lambda x: x > min_threshold]
+        .sort_values(ascending=False)[:top_x]
+        .index.tolist()
+    )
+
+
+def select_feature_columns_with_profiles(profiles, df_feature_types):
+    """
+    Return names of features in scope for the provided profiles.
+
+    Parameters
+    ----------
+    profiles: list
+        List with feature profiles in scope (e.g. 'nature', 'culture', ...).
+    df_feature_types: DataFrame
+        Data set that tells which features belong to which feature profiles.
+
+    Returns
+    -------
+    out: list
+        List with the top x feature names.
+    """
+    return (
+        df_feature_types.set_index("feature_name")[profiles]
+        .sum(axis=1)
+        .loc[lambda x: x > 0]
+        .index.values
+    )
+
+
+def select_features_with_profiles(
+    place_id, feature_profiles, df_features, df_feature_types, top_x=5, *kwargs
+):
+    """
+    Get top_x features for a place, keeping feature profiles into account.
+
+    Parameters
+    ----------
+    place_id: int
+        Place ID to return features for.
+    feature_profiles: list
+        List with feature profiles in scope (e.g. 'nature', 'culture', ...).
+    df_features: DataFrame
+        Data set with feature scores for all places.
+    df_feature_types: DataFrame
+        Data set that tells which features belong to which feature profiles.
+    top_x: int
+        Number of features to return in descending order of feature score.
+
+    Returns
+    -------
+    out: list
+        List with the top x feature names, with features from selected profiles first.
+    """
+    features_inscope = select_feature_columns_with_profiles(
+        feature_profiles, df_feature_types
+    )
+    df_features_inscope = df_features[features_inscope]
+
+    result = select_features(place_id, df_features_inscope, *kwargs)
+
+    if len(result) < top_x:
+        df_features_outscope = df_features.drop(features_inscope, axis=1)
+        result += select_features(place_id, df_features_outscope, *kwargs)[
+            : (top_x - len(result))
+        ]
+
+    return result
