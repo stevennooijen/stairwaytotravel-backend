1e4953c7e75eb360ab31dd6d3bf73e80f02cce54
Steven Nooijen <stevennooijen@godatadriven.com>
2020-09-27 20:31:17 +0200

    Created script for instagram image processing

---
+++ scripts/instagram_image_processing.py
@@ -0,0 +1,81 @@
+import time
+
+import pandas as pd
+from stairway.instagram.image import create_instagram_image
+
+TOP_X_IMAGES = 5
+STEP_SIZE = 1000
+SLEEP_SECONDS = 60 * 5
+
+
+def main(*args):
+    options = parse_args(*args)
+
+    # Read and prep input df
+    df_images = (
+        pd.read_csv(options.input_path)
+        .loc[lambda df: df["index"] < TOP_X_IMAGES]
+        .assign(
+            output_path=lambda df: options.output_path
+            + df["stairway_id"].astype(str)
+            + "-"
+            + df["index"].astype(str)
+            + ".jpeg"
+        )
+    )
+
+    print(f"Number of images to create: {len(df_images)}")
+
+    # Process chuncked up input df with a sleep timer of an hour
+    for i in range(int(options.start_index), len(df_images), STEP_SIZE):
+        print(f"Now doing images: {i}-{i + STEP_SIZE}")
+        df_images.iloc[i : i + STEP_SIZE].apply(process_row, axis=1)
+
+        print(f"Done processing images: {i}-{i + STEP_SIZE}")
+        print("Sleeping ... zzz")
+        time.sleep(SLEEP_SECONDS)
+
+    print("Done!")
+
+
+def parse_args(*args):
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        description="Create Instagram images from list of image locations."
+    )
+    parser.add_argument(
+        "-i",
+        "--input-path",
+        dest="input_path",
+        default="data/flickr/flickr_image_list.csv",
+        help="Path to the place images input file.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-path",
+        dest="output_path",
+        default="/Users/terminator/travel/instagram-images/",
+        help="Path to the output folder.",
+    )
+    parser.add_argument(
+        "-s",
+        "--start-index",
+        dest="start_index",
+        default=0,
+        help="Index of first image to start processing",
+    )
+
+    return parser.parse_args(args=args)
+
+
+def process_row(row):
+    create_instagram_image(
+        row["url_b"], row["name"], row["country"], row["output_path"]
+    )
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(*sys.argv[1:])
