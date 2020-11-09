aa92c6b78cc9bab840e2edd977f98a6da535dc4d
Steven Nooijen <stevennooijen@godatadriven.com>
2020-11-09 21:16:56 +0100

    Add place weights to nearby places recommendations

--- api/resources/nearby.py
+++ api/resources/nearby.py
@@ -3,6 +3,7 @@ from resources.utils.distance import sort_places_by_distance
 
 parser = reqparse.RequestParser()
 parser.add_argument("n_results", type=int, default=12)
+parser.add_argument("seed", type=int, default=1234)
 
 
 class Nearby(Resource):
@@ -21,6 +22,11 @@ class Nearby(Resource):
             places = (
                 self.df
                 .pipe(sort_places_by_distance, place_id)
+                # first get 30 closest places
+                .head(30)
+                # Sort using the number of tokens weight
+                .sample(frac=1, random_state=args["seed"], weights="weight")
+                # return the requested subset
                 .head(args['n_results'])
                 .to_dict(orient="records")
             )
