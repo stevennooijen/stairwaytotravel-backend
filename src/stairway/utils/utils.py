e4266c57d42a44a743cf296041b1afce6b27d219
Steven Nooijen <steven.nooijen@hotmail.com>
2021-03-02 12:41:29 +0100

    Add nb for creating weather data

--- src/stairway/utils/utils.py
+++ src/stairway/utils/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def add_normalized_column(df, column_name):
     """
     Normalize a column and add it to the DataFrame.
@@ -18,3 +21,27 @@ def add_normalized_column(df, column_name):
         df[column_name].max() - df[column_name].min()
     )
     return df
+
+
+# vectorized haversine function
+def vectorized_haversine(
+    lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6371
+):
+    """
+    slightly modified version: of http://stackoverflow.com/a/29546836/2901002
+
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees or in radians)
+
+    All (lat, lon) coordinates must have numeric dtypes and be of equal length.
+
+    """
+    if to_radians:
+        lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
+
+    a = (
+        np.sin((lat2 - lat1) / 2.0) ** 2
+        + np.cos(lat1) * np.cos(lat2) * np.sin((lon2 - lon1) / 2.0) ** 2
+    )
+
+    return earth_radius * 2 * np.arcsin(np.sqrt(a))
